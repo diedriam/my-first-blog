213974326fc4fdea4ab69fa845bcc1e1510c9371
diedriam <diedriam@diedriams-MacBook-Pro.local>
2016-04-30 10:03:04 -0500

    added logout

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/login/$', auth_views.login),
+    url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
