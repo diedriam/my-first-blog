162efbeacad801d6f06f6cd3fd198589f1c537fa
diedriam <diedriam@diedriams-MacBook-Pro.local>
2016-04-30 17:39:03 -0500

    Added possibility to add a comment to user

--- blog/models.py
+++ blog/models.py
@@ -24,6 +24,9 @@ class Post(models.Model):
         self.modified_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
 
@@ -36,7 +39,7 @@ class Comment(models.Model):
 
     def approve(self):
         self.approved_comment = True
-        self.save()
+	self.save()     
 
     def __str__(self):
         return self.text
